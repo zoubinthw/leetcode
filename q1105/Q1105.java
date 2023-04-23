88e133985719a5d8d03d38b97f223cc70fd6e236
zoubinthw <316324334@qq.com>
2023-04-24 01:03:37 +0800

    [1105][binzou][ 填充书架] 动态规划

---
+++ q1105/Q1105.java
@@ -0,0 +1,24 @@
+package q1105;
+
+import java.util.Arrays;
+
+public class Q1105 {
+    public int minHeightShelves(int[][] books, int shelfWidth) {
+        int n = books.length;
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, 1000000);
+        dp[0] = 0;
+
+        for (int i = 0; i < n; ++i) {
+            int curWidth = 0, maxHeight = 0;
+            for (int j = i; j >= 0; --j) {
+                curWidth += books[j][0];
+                if (curWidth > shelfWidth) break;
+                maxHeight = Math.max(maxHeight, books[j][1]);
+                dp[i+1] = Math.min(dp[i+1], dp[j] + maxHeight);
+            }
+        }
+
+        return dp[n];
+    }
+}
