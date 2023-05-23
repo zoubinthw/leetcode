17588f47ff39051ce7cda33c054b70c4b0f091be
zoubinthw <316324334@qq.com>
2023-05-23 18:25:58 +0800

    [1079][binzou][活字印刷][**:表示需要回顾] 中等题目: 似懂非懂，需要再看看

--- Main.java
+++ Main.java
@@ -1,13 +1,10 @@
-import q1073.Q1073;
-import q1090.Q1090;
+import q1079.Q1079;
 
 public class Main {
     public static void main(String[] args) {
-        int[] values = {9,8,8,7,6};
-        int[] labels = {0,0,0,1,1};
-        int numWanted = 3, useLimit = 1;
-        Q1090 q1090 = new Q1090();
-        int res = q1090.largestValsFromLabels(values, labels, numWanted, useLimit);
+        Q1079 q1079 = new Q1079();
+        String tiles = "AB";
+        int res = q1079.numTilePossibilities(tiles);
         System.out.println(res);
     }
 }
